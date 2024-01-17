67cb5b7100e73c5e212355e32744e5a6e28b3c39
Renato Almeida <renatoam@ymail.com>
2024-01-16 23:23:37 -0300

    criação do menu gerenciador

---
+++ gerenciador.py
@@ -0,0 +1,14 @@
+
+while True: 
+    print("\n Menu do Gerenciador de Atividades:")
+    print("1. Adicionar atividade")
+    print("2. Ver Atividades")
+    print("3. Atualizar Atividade")
+    print("4. Completar Atividade")
+    print("5. Deletar tarefas completadas")
+    print("6. Sair")
+
+    escolha = input("Digite a sua opção: ")
+
+    if escolha =="6":
+        break
