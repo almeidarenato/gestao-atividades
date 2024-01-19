13b0878e406cfa45fcd3b9ceaa1ba8542d14ea3d
Renato Almeida <renatoam@ymail.com>
2024-01-18 23:26:17 -0300

    funcionalidade de extraçao de

--- gerenciador.py
+++ gerenciador.py
@@ -12,6 +12,18 @@ def ver_atividades(lista_de_atividades):
         status = "✓" if atividade["completada"] else " "
         nome_atividade = atividade["atividade"]
         print(f"{indice}- [{status}] {nome_atividade}")
+        return
+
+def atualizar_atividade(lista_de_atividades,indice_atividade,novo_nome_atividade):
+    indice = int(indice_atividade) -1
+    print(len(lista_de_atividades))
+    if indice >= 0 and indice< len(lista_de_atividades) :
+            lista_de_atividades[indice]["atividade"] = novo_nome_atividade
+            print(f"Tarefa {indice_atividade} atualizada para {novo_nome_atividade}")
+    else:
+            print("Numero de tarefa inexistente")
+    return
+
 
 lista_de_atividades = []
 
@@ -30,6 +42,11 @@ while True:
         adicionar_atividade(nome_atividade=nome_atividade,lista_de_atividades=lista_de_atividades)
     elif escolha == "2":
         ver_atividades(lista_de_atividades)
+    elif escolha =="3":
+        ver_atividades(lista_de_atividades)
+        indice_atividade = input("Digite o número da atividade: ")
+        novo_nome_atividade = input("Digite o novo nome da atividade: ")
+        atualizar_atividade(lista_de_atividades=lista_de_atividades,indice_atividade=indice_atividade,novo_nome_atividade=novo_nome_atividade)
     elif escolha =="6":
         break
     
